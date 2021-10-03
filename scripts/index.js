6e46060464c7a63fde119bc08c240018d9711885
Ekaterina Yadrikhinskaya <ya78087@yandex.ru>
2021-10-03 19:31:25 +0300

    feat: final edit

---
+++ scripts/index.js
@@ -0,0 +1,35 @@
+const buttonEdit = document.querySelector('.profile__modify');
+const popup = document.querySelector('.popup');
+const popupCross = document.querySelector('.popup__close');
+const profileNameElement = document.querySelector('.profile__name');
+const profileDescriptionElement  = document.querySelector('.profile__description');
+const inputName = document.querySelector('.form-edit__input_type_name');
+const inputAbout = document.querySelector('.form-edit__input_type_profession');
+const formProfileEdit = document.querySelector('.form-edit');
+
+function closePopup () {
+  popup.classList.remove('popup_opened');
+}
+
+function openPopup () {
+  popup.classList.add('popup_opened');
+}
+
+function formSubmitHandler (evt) {
+  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+  profileNameElement.textContent = inputName.value;
+  profileDescriptionElement.textContent = inputAbout.value;
+  closePopup();
+}
+
+formProfileEdit.addEventListener('submit', formSubmitHandler);
+
+buttonEdit.addEventListener('click', function () {
+  inputName.value = profileNameElement.textContent;
+  inputAbout.value = profileDescriptionElement.textContent;
+  openPopup();
+});
+
+popupCross.addEventListener('click', function () {
+  closePopup();
+ });
