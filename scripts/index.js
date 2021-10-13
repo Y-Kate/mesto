29247f6672d236f62a0e954f7033cd0b79706a12
Ekaterina Yadrikhinskaya <ya78087@yandex.ru>
2021-10-13 17:29:04 +0300

    1

--- scripts/index.js
+++ scripts/index.js
@@ -6,6 +6,49 @@ const profileDescriptionElement  = document.querySelector('.profile__description
 const inputName = document.querySelector('.form-edit__input_type_name');
 const inputAbout = document.querySelector('.form-edit__input_type_profession');
 const formProfileEdit = document.querySelector('.form-edit');
+//изменение из 6 спринта в const
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+const formCard = document.querySelector('.card');
+function rendorCard (card) {
+  return `
+  <img src="${card.link}" alt="${card.name}" class="card__image">
+  <div class="card__description">
+    <h2 class="card__title">${card.name}</h2>
+    <button type="button" class="card__button-like" aria-label="Нравится"></button>
+  </div>
+  `;
+};
+
+const cards = initialCards.map((initialCard) => rendorCard(initialCard));
+
+console.log(cards);
+
 
 function closePopup () {
   popup.classList.remove('popup_opened');
@@ -27,7 +70,7 @@ function handleClickButtonEdit() {
   inputAbout.value = profileDescriptionElement.textContent;
   openPopup();
 }
-
+//изменение из 6 спринта в функциях
 formProfileEdit.addEventListener('submit', formSubmitHandler);
 
 buttonEdit.addEventListener('click', handleClickButtonEdit);
